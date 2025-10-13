7fdc19cf143334b7b48ddeb1f689b4396f61ab25
Raddin Pratama Rachmat <142906084+Radd-25@users.noreply.github.com>
2025-10-13 09:57:59 +0700

    Add too many files

---
+++ Java/coba1.java
@@ -0,0 +1,12 @@
+class coba1 {
+ public static void main(String[] args) {
+ String nim = "I-25XXX";
+ String nama = "Raddin Pr";
+ String umur = "Rahasia";
+ String profesi = "Mahasiswa";
+ System.out.println(nim);
+ System.out.println(nama);
+ System.out.println(umur);
+ System.out.println(profesi);
+ }
+}
