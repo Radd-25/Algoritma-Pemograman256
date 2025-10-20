8db8968e7c5a37ccecf10f9a11e39824550707a3
Raddin Pratama Rachmat <r4dd1nk4sep@gmail.com>
2025-10-20 22:51:33 +0700

    adding java assignment

---
+++ Java/booleaan.java
@@ -0,0 +1,8 @@
+public class booleaan {
+    public static void main(String[] args) {
+        boolean ApakahDosenAlproKeren = true;
+        boolean ApakahDosenAlproJelek = false;
+        System.out.println("Nilai ApakahDosenAlproKeren = " + ApakahDosenAlproKeren);
+        System.out.println("Nilai ApakahDosenAlproJelek = " + ApakahDosenAlproJelek);
+    }
+}
