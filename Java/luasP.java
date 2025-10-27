a082172072280e3db1b582e31cfbac336e72dbf2
Raddin Pratama Rachmat <142906084+Radd-25@users.noreply.github.com>
2025-10-27 09:51:44 +0700

    Create luasP.java

---
+++ Java/luasP.java
@@ -0,0 +1,8 @@
+public class luasP {
+    public static void main(String[] args) {
+        int s1 = 10;
+        int s2 = 5;
+        int Lpersegi = s1 * s2;
+        System.out.println("Luas Persegi Panjang: " + Lpersegi);
+    }
+}
